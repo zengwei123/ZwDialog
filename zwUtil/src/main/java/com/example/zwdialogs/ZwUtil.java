2ad36738cc3b8074e0f70cbea584064aeca9f14f
zengwei <13085486819@163.com>
2018-09-19 14:46:18 +0800

    动画修改

--- zwUtil/src/main/java/com/example/zwdialogs/ZwUtil.java
+++ zwUtil/src/main/java/com/example/zwdialogs/ZwUtil.java
@@ -51,15 +51,29 @@ public class ZwUtil {
      * @param animStr  动画类型
      * @param floats   属性
      */
-    public static void AnimatorFloat(View view,int time,String animStr,float... floats){
+    public static void AnimatorFloat(View view,int time,int StartDelay,int i,boolean b,String animStr,float... floats){
         ObjectAnimator objectAnimator= ObjectAnimator.ofFloat(view,animStr, floats);       //透明度
         objectAnimator.setEvaluator(new FloatEvaluator());    //设置一个估值器，常用的有ArgbEvaluator\IntEvaluator\FloatEvaluator
+        objectAnimator.setRepeatCount(i);
+        if(b){
+            objectAnimator.setRepeatMode(ObjectAnimator.REVERSE);    //重复模式 REVERSE逆向重复  REPEAT 连续重复
+        }else {
+            objectAnimator.setRepeatMode(ObjectAnimator.RESTART);    //重复模式 REVERSE逆向重复  REPEAT 连续重复
+        }
         objectAnimator.setDuration(time);
+        objectAnimator.setStartDelay(StartDelay);
         objectAnimator.start();
     }
-    public static void AnimatorInt(View view,int time,String animStr,int... ints){
+    public static void AnimatorInt(View view,int time,int StartDelay,int i,boolean b,String animStr,int... ints){
         ObjectAnimator objectAnimator= ObjectAnimator.ofInt(view,animStr, ints);       //透明度
         objectAnimator.setEvaluator(new IntEvaluator());    //设置一个估值器，常用的有ArgbEvaluator\IntEvaluator\FloatEvaluator
+        objectAnimator.setRepeatCount(i);
+        if(b){
+            objectAnimator.setRepeatMode(ObjectAnimator.REVERSE);    //重复模式 REVERSE逆向重复  REPEAT 连续重复
+        }else {
+            objectAnimator.setRepeatMode(ObjectAnimator.RESTART);    //重复模式 REVERSE逆向重复  REPEAT 连续重复
+        }
+        objectAnimator.setDuration(time);
         objectAnimator.setDuration(time);
         objectAnimator.start();
     }
